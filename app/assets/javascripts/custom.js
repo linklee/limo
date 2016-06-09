90abd1813cfc71c28d75ec73c0760f90936e70c3
jonstark <nevernight721@gmail.com>
2016-06-09 18:56:22 +0500

    l

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -1,8 +1,8 @@
-var ready;
+var ready, maps_init;
 ready = function() {
 
-$(".select-book-type").change(function(){
-	var term = $(this).val();
+	$(".select-book-type").change(function(){
+		var term = $(this).val();
 	//hide all inputs
 	$('.drop-off-wrap').addClass('hidden');
 	$('.pick-up-wrap').addClass('hidden');
@@ -22,7 +22,7 @@ $(".select-book-type").change(function(){
 		$('.pick-up-wrap').removeClass('hidden');
 	}
 	if (term === 'Book by the Hour') {
-	
+
 	}
 	if (term === 'Address to Address One Way') {
 		$('.drop-off-wrap').removeClass('hidden');
@@ -34,8 +34,37 @@ $(".select-book-type").change(function(){
 	}
 });
 
+//initialize google maps
 
-};
+//we need google maps only if google.js is presented. So
 
+if(typeof google == 'undefined') { return; }
+
+var myCenter=new google.maps.LatLng(34.1571367,-118.294432,17);
+function initialize()
+{
+	var mapProp = {
+		center:myCenter,
+		zoom:15,
+		mapTypeId:google.maps.MapTypeId.ROADMAP,
+		scrollwheel: false
+
+	};
+
+	var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
+
+
+	var marker=new google.maps.Marker({
+		position:myCenter,
+	});
+
+	marker.setMap(map);
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
+
+};//end ready function
+
+//on documentload and on turbolinks load call ready function (it is calling just once)
 $(document).ready(ready);
 $(document).on('page:load', ready);
